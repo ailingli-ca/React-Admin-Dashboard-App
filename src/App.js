abbd08163bb9f0785def4cd2fef73d4956d01660
Ailing Li <ailingli@Ailings-MacBook.local>
2023-02-17 14:47:47 -0800

    Theme - Light & Dark Mode & Topbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { ColorModeContext, useMode} from './theme';
+import { CssBaseline, ThemeProvider} from "@mui/material";
+import { Routes, Route} from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+
+
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+
+  return (
+  <ColorModeContext.provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="app">
+        <main className = "content">
+          <Topbar />
+        </main>        
+      </div>;
+    </ThemeProvider>
+  </ColorModeContext.provider>
+  );
 }
 
 export default App;
